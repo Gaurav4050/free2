0c6949aa2f215012421b3f18eae266c70f2f3756
dhiraj-source <80595948+dhiraj-source@users.noreply.github.com>
2023-12-08 15:36:44 +0530

    "boiler-plate"

--- src/app.js
+++ src/app.js
@@ -1,50 +1,19 @@
-
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header"; 
-import Footer from "./components/Footer"; 
-import NewNote from "./components/NewNote"; 
-import CreateCard from "./components/minors/CreateCard"; 
-import ls from "local-storage";
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/home/Home";
+import LoginForm from "./components/LoginForm/Index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const nullNote = { id: 9999999, title: "NULL", note: "NULL" };
-  const storedNotes = JSON.parse(ls.get("notes")) || [nullNote];
 
-  const [notes, setNotes] = useState(storedNotes);
-
-  useEffect(() => {
-    ls("notes", JSON.stringify(notes.filter(note => note.id !== 9999999)));
-  }, [notes]);
-
-  const addNote = (title, content) => {
-    const newId = Math.round(Math.random() * 100);
-    setNotes(prevNotes => [
-      ...prevNotes,
-      { id: newId, title, note: content },
-    ]);
-  };
-
-  const deleteNote = id => {
-    setNotes(prevNotes => prevNotes.filter(note => note.id !== id));
-  };
-
-  const saveNote = (id, content) => {
-    setNotes(prevNotes =>
-      prevNotes.map(note =>
-        note.id === id ? { ...note, note: content } : note
-      )
-    );
-  };
 
   return (
-    <div>
-      <Header />
-      <NewNote add={addNote} />
-      <div className="row">
-        {notes.map(note => CreateCard(note, deleteNote, saveNote))}
-      </div>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginForm/>}  />
+        <Route path ='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/home/Home.js
@@ -0,0 +1,55 @@
+
+import React, { useState, useEffect } from "react";
+// import NewNote from "./components/NewNote";
+import NewNote from '../NewNote'
+import Header from "../Header";
+// import CreateCard from "./components/minors/CreateCard";
+import CreateCard from '../minors/CreateCard'
+import ls from "local-storage";
+import Footer from "../Footer";
+
+
+const Home = () => {
+    const nullNote = { id: 9999999, title: "NULL", note: "NULL" };
+    const storedNotes = JSON.parse(ls.get("notes")) || [nullNote];
+
+    const [notes, setNotes] = useState(storedNotes);
+
+    useEffect(() => {
+        ls("notes", JSON.stringify(notes.filter(note => note.id !== 9999999)));
+    }, [notes]);
+
+    const addNote = (title, content) => {
+        const newId = Math.round(Math.random() * 100);
+        setNotes(prevNotes => [
+            ...prevNotes,
+            { id: newId, title, note: content },
+        ]);
+    };
+
+    const deleteNote = id => {
+        setNotes(prevNotes => prevNotes.filter(note => note.id !== id));
+    };
+
+    const saveNote = (id, content) => {
+        setNotes(prevNotes =>
+            prevNotes.map(note =>
+                note.id === id ? { ...note, note: content } : note
+            )
+        );
+    };
+    return (
+        <>
+            <Header />
+
+            <NewNote add={addNote} />
+            <div className="row">
+                {notes.map(note => CreateCard(note, deleteNote, saveNote))}
+            </div>
+            <Footer />
+
+        </>
+    )
+}
+
+export default Home
